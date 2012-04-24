61e2ac0f5501f7f39cf9be4cbcab971ff41b598b
Toomas Vahter <toomas.vahter@gmail.com>
2012-04-24 18:03:20 +0300

    Added MIT license and removed resolutionviewcontroller*

--- Lookout/MainWindowController.m
+++ Lookout/MainWindowController.m
@@ -5,10 +5,28 @@
 //  Created by Toomas Vahter on 06.04.12.
 //  Copyright (c) 2012 Toomas Vahter. All rights reserved.
 //
+//  This content is released under the MIT License (http://www.opensource.org/licenses/mit-license.php).
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import "MainWindowController.h"
 #import "CaptureViewController.h"
-#import "ResolutionViewController.h"
 
 @implementation MainWindowController
 
@@ -59,7 +77,7 @@
 	{
 		resolutionPopover = [[NSPopover alloc] init];
 		resolutionPopover.behavior = NSPopoverBehaviorSemitransient;
-		[resolutionPopover setContentViewController:[[ResolutionViewController alloc] init]];
+		[resolutionPopover setContentViewController:[[NSViewController alloc] initWithNibName:@"ResolutionView" bundle:nil]];
 		[resolutionPopover setContentSize:NSMakeSize(327.0, 62.0)];
 	}
 	
